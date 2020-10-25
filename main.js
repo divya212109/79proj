c39bec42c41b20a9add15804017ea0ca30aebcb0
divya212109 <73348305+divya212109@users.noreply.github.com>
2020-10-25 16:33:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+name_of_the_menu_list_array=[];
+
+function GetMenu()
+{
+    menu_list_array = ["Cheese Pizza",
+"Pepparoni Pizza", "Veggie Pizza", "Extra Cheese Pizza", "Extra Pepparoni Pizza"]
+}
+name_of_the_student_array.push(name_of_the_menu_list_array);
+
+document.getElementById("display_name").innerHTML = name_of_the_student_array;
+document.getElementById("Recommend a new Pizza button").style.display = "none";
+document.getElementById("Show Menu_button").style.display = "inline-block";
